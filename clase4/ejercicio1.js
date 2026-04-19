b71093c942283b5c984fd39063bc64249d1d62cf
valentinaalaniz <alanizvalentina07@gmail.com>
2026-04-18 22:17:23 -0300

    ejercicios clase js

---
+++ clase4/ejercicio1.js
@@ -0,0 +1,7 @@
+const producto = 'iphone15';
+const precio = 20000;
+const disponible = false;
+
+console.log(
+  `El ${producto} cuesta ${precio} y su disponibilidad es: ${disponible}`
+);
---
+++ clase4/ejercicio3.js
@@ -0,0 +1,5 @@
+const compras = ['yerba', 'cafe', 'mermelada'];
+
+for(let i =0; i < compras.length; i++){
+    console.log (`¡No te olvides de comprar ${compras[i]}!`)
+}
